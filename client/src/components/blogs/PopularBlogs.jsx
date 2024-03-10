82de21cf66daba3d7943105a2655afdb51324954
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-03-10 15:27:39 +0600

    create useFetchPopularBlogs hook to fetch popular blogs

--- client/src/components/blogs/PopularBlogs.jsx
+++ client/src/components/blogs/PopularBlogs.jsx
@@ -1,43 +1,8 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import { actions } from '../../actions';
-import useBlog from '../../hooks/blog/useBlog';
+import useFetchPopularBlogs from '../../hooks/blog/useFetchPopularBlogs';
 import PopularBlogList from './PopularBlogList';
 
 const PopularBlogs = () => {
-  const { blogState, blogDispatch } = useBlog();
-  const { popularBlogs } = blogState || {};
-
-  useEffect(() => {
-    let ignore = false;
-    blogDispatch({ type: actions.blog.DATA_FETCHING });
-
-    const fetchPopularBlogs = async () => {
-      try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_SERVER_BASE_URL}/blogs/popular`
-        );
-
-        if (response.status === 200 && !ignore) {
-          blogDispatch({
-            type: actions.blog.POPULAR_BLOG_DATA_FETCHED,
-            data: response.data.blogs,
-          });
-        }
-      } catch (error) {
-        blogDispatch({
-          type: actions.blog.DATA_FETCH_ERROR,
-          error: error.message,
-        });
-      }
-    };
-
-    fetchPopularBlogs();
-
-    return () => {
-      ignore = true;
-    };
-  }, []);
+  const { popularBlogs } = useFetchPopularBlogs();
 
   return (
     <div className="sidebar-card">
---
+++ client/src/hooks/blog/useFetchPopularBlogs.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { actions } from '../../actions';
+import useBlog from './useBlog';
+
+const useFetchPopularBlogs = () => {
+  const { blogState, blogDispatch } = useBlog();
+  const { popularBlogs } = blogState || {};
+
+  useEffect(() => {
+    let ignore = false;
+    blogDispatch({ type: actions.blog.DATA_FETCHING });
+
+    const fetchPopularBlogs = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/blogs/popular`
+        );
+
+        if (response.status === 200 && !ignore) {
+          blogDispatch({
+            type: actions.blog.POPULAR_BLOG_DATA_FETCHED,
+            data: response.data.blogs,
+          });
+        }
+      } catch (error) {
+        blogDispatch({
+          type: actions.blog.DATA_FETCH_ERROR,
+          error: error.message,
+        });
+      }
+    };
+
+    fetchPopularBlogs();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return { popularBlogs };
+};
+
+export default useFetchPopularBlogs;
