588f2f1bebb12903a93a6a19d53b8a77c3b84850
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-03-11 15:48:19 +0600

    modify ErrorPage

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -4,18 +4,25 @@ const ErrorPage = () => {
   const error = useRouteError();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i className="text-red-500">{error.statusText || error.message}</i>
-      </p>
-      <Link to="/">
-        <button className="text-white font-semibold bg-[#14b8a6] px-3 py-2.5 rounded-md">
-          Back To Home
-        </button>
-      </Link>
-    </div>
+    <section className="flex items-center h-screen p-16 dark:dark:bg-[#030317] dark:dark:text-white">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="font-extrabold mb-7 text-9xl">
+            <span className="sr-only">Error</span>
+            {error?.status}
+          </h2>
+          <p className="mb-12 text-2xl font-semibold md:text-3xl">
+            Sorry, couldn`t find this page.
+          </p>
+          <Link
+            to="/"
+            className="px-8 py-3 font-semibold rounded dark:dark:bg-[#14b8a6]  dark:dark:text-white"
+          >
+            Back to Homepage
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
